16e8b4c7f6edab9d73c621e3d46513a5fe834e75
Mike Heft <mikeheft@gmail.com>
2017-07-15 14:52:05 -0600

    work on player input

---
+++ lib/messages.rb
@@ -0,0 +1,29 @@
+module Messages
+  def ask_for_ship_1_coords
+    puts "Enter your 2 unit ship's coordinates now: (ie A1)"
+    print "> "
+  end
+
+  def exit_message
+    rate = 250
+    voice = "Ralph"
+    goodbye = " I'm afraid. I'm afraid, Dave. Dave, my mind is going. I can feel it. I can feel it. My mind is going. There is no question about it. I can feel it. I can feel it. I can feel it. I'm a fraaaid. Good afternoon, gentlemen. I am a Hal 9000 computer. I became operational at the H.A.L. plant in Urbana, Illinois on the 12th of January 1992. My instructor was Mr. Langley, and he taught me to sing a song. If you'd like to hear it I can sing it for you. "
+    `say -r #{rate} -v #{voice} #{goodbye}`
+  end
+
+  def song
+     pid = fork{ exec 'afplay', "../Rick Astley - Never Gonna Give You Up.mp3" }
+  end
+
+  def quit_commands(command)
+    %w(q Q quit Quit QUIT).include?(command)
+  end
+
+  def no_commands(command)
+    %w(n N no NO No).include?(command)
+  end
+
+  def yes_commands(command)
+    %w(y Y yes Yes YES).include?(command)
+  end
+end
--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,11 @@
+require './lib/messages'
+
 class Player
+  include Messages
+
   attr_reader :board
   attr_accessor :ship_1, :ship_2
-  
+
   def initialize
     @board = [["A1","A2","A3","A4"],
               ["B1","B2","B3","B4"],
@@ -10,4 +14,55 @@ class Player
     @ship_1 = []
     @ship_2 = []
   end
+
+  def second_coord
+    {"A1"=>["A2","B1"], "A2"=>["A1","A3","B2"], "A3"=>["A2","A4","B3"],"A4"=>["B4"],
+     "B1"=>["B2","A1","C1"], "B2"=>["B1","B3","A2","C2"], "B3"=>["A3","B2","B4","C3"], "B4"=>["A4","B3","C4"],
+     "C1"=>["B1","C2","D1"], "C2"=>["C1","C3","B2","D2"], "C3"=>["B3","C2","C4","D3"], "C4"=>["B4","C3","D4"],
+     "D1"=>["C1","D2"], "D2"=>["D1","D3","C2"], "D3"=>["C3","D2","D4"], "D4"=>["C4"]}
+  end
+
+  def ship_1_placement(command)
+    ask_for_ship_1_coords
+    get_coords
+    if quit_commands(command)
+      exit_message
+      exit
+    else
+      start_commands()
+    end
+  end
+
+  def third_coord
+    { "A1"=>[["A2","A3"],["B1","C1"]], "A2"=>[["A1","A3"],["B2","C2"]],
+      "A3"=>[["A2","A4"],["B3","C3"]], "A4"=>[["A2","A3"],["B4","C4"]],
+      "B1"=>[["A1","C1"],["B2","B3"]], "B2"=>[["B1","B3"],["A2","C2"]],
+      "B3"=>[["B2","B4"],["A3","C3"]], "B4"=>[["B2","B3"],["A4","C4"]],
+      "C1"=>[["B1","D1"],["C2","C3"]], "C2"=>[["B2","D2"],["C1","C3"]],
+      "C3"=>[["B3","D3"],["C2","C4"]], "C4"=>[["B4","D4"],["C2","C3"]],
+      "D1"=>[["B1","C1"],["D2","D3"]], "D2"=>[["D1","D3"],["B2","C2"]],
+      "D3"=>[["D2","D4"],["C3","B3"]], "D4"=>[["B4","C4"],["D2","D3"]]}
+  end
+
+  def get_coords
+    input = gets.chomp
+    messages.change_commands(input)
+    messages.set_commands
+  end
+
+  def change_commands(command)
+    commands = command.split
+    second_entry(commands)
+    long_third_command(commands,commands[1])
+    @first_command = commands[0] if commands[0] != nil
+    @second_command = commands[1] if commands[1] != nil
+    @third_command = commands[2] if commands[2] != nil
+  end
+
+  def set_commands
+    @first_command  = commands.first_command
+    @second_command = commands.second_command if commands.second_command != nil
+    @third_command  = commands.third_command if commands.third_command != nil
+  end
+
 end
