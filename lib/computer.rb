5e8f535d7690724839e0ec745a3dd904dcbe1bb4
Mike Heft <mikeheft@gmail.com>
2017-07-15 10:19:28 -0600

    work on 2 unit ship placement

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,10 +1,17 @@
 class Computer
-  attr_reader :board
+  attr_reader :board, :ship_1, :ship_2
 
   def initialize
     @board = [["A1","A2","A3","A4"],
               ["B1","B2","B3","B4"],
               ["C1","C2","C3","C4"],
               ["D1","D2","D3","D4"]]
+    @ship_1 = []
+    @ship_2 = []
+  end
+
+  def second_coord_selection
+    {"A1"=>["A2","B1"}, "A2"=>["A3","B2"], "A3"=>["A4","B3"],"A4"=>["B4"],
+     "B1"=>["B2","A1"}, "B2"=>["B3","B2"], "B3"=>["B4","B3"],"B4"=>["B4"],
   end
 end
--- test/computer_test.rb
+++ test/computer_test.rb
@@ -15,4 +15,25 @@ class ComputerTest < Minitest::Test
   def test_it_starts_with_a_board
     assert_instance_of Array, computer.board
   end
+
+  def test_it_starts_with__ship
+    assert_instance_of Array, computer.ship_1
+  end
+
+  def test_ship_1_is_empty_by_default
+    assert_equal 0, computer.ship_1.length
+  end
+
+  def test_it_starts_with_a_second_ship
+    assert_instance_of Array, computer.ship_2
+  end
+
+  def test_second_ship_start_empty
+    assert_equal 0, computer.ship_2.length
+  end
+
+  def test_it_can_make_a_two_unit_ship
+    assert_equal 2, computer.ship.length
+  end
+  
 end
